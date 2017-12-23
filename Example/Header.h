7f39fe9b454acc78ac21a42d62d47d9a3326f8c5
KhacSan <khacsan2511@gmail.com>
2017-12-23 17:09:46 +0700

    Add project files.

---
+++ Example/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+double *plotting1DArrays(std::string function, double, double);
+
+
+double *plotting2DArrays(std::string function, double, double,
+	double, double, double, double, double);
+
+double *plotting3DArrays(std::string function, double, double,
+	double, double, double, double, double, double, double);
+
